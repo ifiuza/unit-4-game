96b53a2110147ad509fc134d2f5ead2b22ca1e0b
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-06-03 20:46:50 -0600

    last update/missing js now

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    //Global variables
+    var wins = 0;
+    var losses = 0;
+    $(".wins-text").text("Wins: " + wins);
+    $(".losses-text").text("Losses: " + losses);
+}); 
+
